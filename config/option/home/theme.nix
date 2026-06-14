a0f5370329b4e761b2c51b2518c408a29419850f
Simon Kennedy <sffjunkie+code@gmail.com>
2026-06-14 10:11:12 +0100

    update: theme options

--- config/option/home/theme.nix
+++ config/option/home/theme.nix
@@ -7,22 +7,44 @@ let
   inherit (lib) mkOption types;
 in
 {
-  options.looniversity.theme = {
+  options.looniversity.user.theme = {
     base16 = mkOption {
       type = types.str;
-      default = osConfig.looniversity.theme.base16;
+      default = osConfig.looniversity.site.theme.base16;
       description = "User base16 color scheme";
     };
 
     catppuccin = {
       flavor = mkOption {
         type = types.str;
-        default = osConfig.looniversity.theme.catppuccin.flavor;
+        default = osConfig.looniversity.site.theme.catppuccin.flavor;
+        description = "catppuccin flavor";
       };
 
       accent = mkOption {
         type = types.str;
-        default = osConfig.looniversity.theme.catppuccin.accent;
+        default = osConfig.looniversity.site.theme.catppuccin.accent;
+        description = "catppuccin accent color";
+      };
+    };
+
+    cursor = {
+      package = mkOption {
+        description = "Cursor package";
+        type = types.nullOr types.package;
+        default = null;
+      };
+
+      name = mkOption {
+        description = "Cursor name";
+        type = types.nullOr types.str;
+        default = null;
+      };
+
+      size = mkOption {
+        description = "Cursor size";
+        type = types.int;
+        default = -1;
       };
     };
 
@@ -30,6 +52,7 @@ in
       image = mkOption {
         type = types.nullOr types.path;
         default = null;
+        description = "lock screen background image";
       };
     };
   };
