393849f63c51b683bb7a1984f8a10c6f6f5e6761
Simon Kennedy <sffjunkie+code@gmail.com>
2026-06-14 20:24:53 +0100

    change: remove `package` attr as not required

--- config/user/user/sdk/home/config/lde/cursor.nix
+++ config/user/user/sdk/home/config/lde/cursor.nix
@@ -8,7 +8,6 @@ let
 in
 formatter.generate "desktop-cursor" {
   cursor = {
-    package = if (cfg.name != null) then cfg.package else config.home.pointerCursor.package;
     name = if (cfg.name != null) then cfg.name else config.home.pointerCursor.name;
     size = if (cfg.size != -1) then cfg.size else config.home.pointerCursor.size;
   };
