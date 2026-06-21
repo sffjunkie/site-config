8e53740502ee8eece556c502f34f2f0f1a280bd1
Simon Kennedy <sffjunkie+code@gmail.com>
2026-06-21 14:58:30 +0100

    add: policies for extensions and search engines

--- config/module/home/browser/firefox.nix
+++ config/module/home/browser/firefox.nix
@@ -16,6 +16,50 @@ in
     programs.firefox = {
       enable = true;
       configPath = "${config.xdg.configHome}/mozilla/firefox";
+      policies = {
+        # To add additional extensions, find it on addons.mozilla.org, find
+        # the short ID in the url (like https://addons.mozilla.org/en-US/firefox/addon/!SHORT_ID!/)
+        # Then, download the XPI by filling it in to the install_url template, unzip it,
+        # run `jq .browser_specific_settings.gecko.id manifest.json` or
+        # `jq .applications.gecko.id manifest.json` to get the UUID
+        ExtensionSettings =
+          with builtins;
+          let
+            extension = shortId: uuid: {
+              name = uuid;
+              value = {
+                install_url = "https://addons.mozilla.org/en-US/firefox/downloads/latest/${shortId}/latest.xpi";
+                installation_mode = "normal_installed";
+              };
+            };
+          in
+          listToAttrs [
+            (extension "clipper@obsidian.md" "clipper@obsidian.md")
+            (extension "keepassxc-browser@keepassxc.org" "keepassxc-browser@keepassxc.org")
+          ];
+
+        SearchEngines = {
+          Add = [
+            {
+              Alias = "@np";
+              Description = "Search in NixOS Packages";
+              IconURL = "https://nixos.org/favicon.png";
+              Method = "GET";
+              Name = "NixOS Packages";
+              URLTemplate = "https://search.nixos.org/packages?from=0&size=200&sort=relevance&type=packages&query={searchTerms}";
+            }
+            {
+              Alias = "@no";
+              Description = "Search in NixOS Options";
+              IconURL = "https://nixos.org/favicon.png";
+              Method = "GET";
+              Name = "NixOS Options";
+              URLTemplate = "https://search.nixos.org/options?from=0&size=200&sort=relevance&type=packages&query={searchTerms}";
+            }
+          ];
+        };
+      };
+
       profiles = {
         default = {
           extensions.force = true;
