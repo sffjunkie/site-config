e59391aace5eb2c9845a823aedeba479b329a5e6
Simon Kennedy <sffjunkie+code@gmail.com>
2026-06-21 17:26:23 +0100

    chore: remove unused code

--- config/module/host/desktop/environment.nix
+++ config/module/host/desktop/environment.nix
@@ -15,9 +15,6 @@ in
   config = mkIf cfg.enable {
     environment.sessionVariables = {
       _JAVA_AWT_WM_NONREPARENTING = "1";
-      # MOZ_ENABLE_WAYLAND = "1";
-      # NIXOS_OZONE_WL = "1";
-      # QT_QPA_PLATFORM = "wayland";
       QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
       SDL_VIDEODRIVER = "wayland,x11,windows";
     };
