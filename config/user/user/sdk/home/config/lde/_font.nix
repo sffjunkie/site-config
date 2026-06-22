ae996760e67ec8b2b7fa65bb673271f0a8a54cb3
Simon Kennedy <sffjunkie+code@gmail.com>
2026-06-22 12:35:42 +0100

    add: convert base font size to pixels

--- config/user/user/sdk/home/config/lde/_font.nix
+++ config/user/user/sdk/home/config/lde/_font.nix
@@ -7,15 +7,15 @@ formatter.generate "desktop-font" {
   font = {
     content = {
       family = config.looniversity.user.theme.font.content.family;
-      size = 16;
+      size = builtins.ceil (config.looniversity.user.theme.font.content.size * 1.33333);
     };
     ui = {
       family = config.looniversity.user.theme.font.ui.family;
-      size = 16;
+      size = builtins.ceil (config.looniversity.user.theme.font.ui.size * 1.33333);
     };
     icon = {
       family = config.looniversity.user.theme.font.icon.family;
-      size = 22;
+      size = builtins.ceil (config.looniversity.user.theme.font.icon.size * 1.33333 * 1.5);
     };
   };
 }
